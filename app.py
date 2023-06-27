fe6e7bc0a09d1d4ac04e32e60aa87829f0122919
David. S <stephan.david93@googlemail.com>
2023-06-27 10:17:38 +0200

    Dash 3

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from dash import Dash, html, dcc
+from dash.dependencies import Input, Output
 import numpy as np
 import pandas as pd
 import plotly.express as px
@@ -31,10 +32,22 @@ df['Zeitdifferenzen'] = df['Zeitstempel'].diff().dt.total_seconds().shift(-1)
 df['Strecke'] = df['Zeitdifferenzen'] * df['Geschwindigkeit']
 
 """
+    Berechnen der Durschnittsgeschwindigkeit
+"""
+average_speed = df['Strecke'].sum() / total_drive_time
+
+"""
+Grafische Aufbereitung der Strecke
+"""
+df['Strecke_Grafik'] = df['Strecke'].shift(1).cumsum()
+df['Strecke_Grafik'].iloc[0] = 0
+print(df)
+
+
+"""
     Webseite erstellen
 """
 app = Dash()
-fig = px.line(df, x=df['Zeitstempel'], y=df['Geschwindigkeit'])
 
 app.layout = html.Div(
     children=[
@@ -44,8 +57,24 @@ app.layout = html.Div(
     html.P(children='Minimale Geschwindigkeit: ' + str(df['Geschwindigkeit'].min())),
     html.P(children='Gesamtfahrzeit: ' + str(total_drive_time)),
     html.P(children='Gesamtstrecke: ' + str(df['Strecke'].sum())),
-    html.P(children='Durchschnittsgeschwindigkeit: ' + str(df['Strecke'].sum())), # Hier fehlt noch was !!!
-    dcc.Graph(figure=fig)])
+    html.P(children='Durchschnittsgeschwindigkeit: ' + str(average_speed)),
+    dcc.Dropdown(id='dropdown', options=[
+        {'label': 'Geschwindigkeit', 'value': 'Geschwindigkeit'},
+        {'label': 'Fahrtrichtung', 'value': 'Fahrtrichtung'},
+        {'label': 'Lenkwinkel', 'value': 'Lenkwinkel'},
+        {'label': 'Abstand', 'value': 'Abstand'},
+        {'label': 'Strecke', 'value': 'Strecke_Grafik'},
+    ], value = 'Geschwindigkeit' ),
+    dcc.Graph(id='line_plot')])
+
+@app.callback(
+    Output(component_id='line_plot', component_property='figure'),
+    Input(component_id='dropdown', component_property='value'))
+def graph_update(value_of_input_component):
+    print(value_of_input_component)
+    fig = px.line(df, x=df['Zeitstempel'], y=df[value_of_input_component])
+    return fig
+
 
 if __name__ == '__main__':
-    app.run_server(host = '192.168.178.66', port=8080, debug=False)
+    app.run_server(host = '127.0.0.1', port=8080, debug=False)
