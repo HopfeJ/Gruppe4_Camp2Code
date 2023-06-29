4f0a85c2e5ab99f1de56f3d45d961e813087bf35
itleicht <descartesbs@web.de>
2023-06-29 15:13:04 +0200

    Update Video und sonstiges

--- fahrparcours_7.py
+++ fahrparcours_7.py
@@ -2,15 +2,20 @@ import time
 from sensor_car import SensorCar
 
 
-def fahrparcours_6(my_car):
+def fahrparcours_7(my_car):
     my_car.create_data_table()
     my_car.drive(40,1) # Volle Fahrt voraus
     my_car.steering_angle = 90 # Räder gerade stellen
     while True:
-        fertig = my_car.drive_with_line_follower(6)
+        fertig = my_car.drive_with_line_follower(7)
         if fertig:
             break
-
+        if my_car.distance < 15 and my_car.distance > 0:
+            print(f'Ergebnis Stop (Abstand Hindernis) {my_car.distance}')
+            my_car.stop()
+            my_car.save_data(my_car.speed, my_car.direction, my_car.steering_angle, my_car.distance, 3, my_car.sensordaten)
+            break
+        
 if __name__ == "__main__":
     my_car = SensorCar()
-    fahrparcours_6(my_car)  
+    fahrparcours_7(my_car)  
