97c149d2e1b1cb6c05d37820831f806a15a674e8
HopfeJ <jan198686@gmx.de>
2023-09-28 00:09:51 +0200

    Jan

--- Jan/Phase2/deepcar.py
+++ Jan/Phase2/deepcar.py
@@ -13,20 +13,20 @@ class DeepCar(CamCar):
     def __init__(self):
         super().__init__()
 
-    def load_model(self):
-        path_to_model_file = './DEMO_MODEL'
+    def load_model(self):                                               # Laden des models
+        path_to_model_file = './DEMO_MODEL'                             # Hier wird der Pfad angegeben in dem sich das Model befindet
         self.model_loaded = tf.keras.models.load_model(path_to_model_file)
-        #
+        
 
     def calculate_angle_from_nn(self, img):
         img_np_array = np.array([img]) 
-        evaluated = self.model_loaded(img_np_array).numpy()
+        evaluated = self.model_loaded(img_np_array).numpy()         #Hier wird das Bild übergeben und ausgewertet
         print(round(evaluated[0][0]))
         return round(evaluated[0][0])
         
 
     def resize_picture(self,img):
-        dim = (320,150)
+        dim = (320,150)                         # bildgröße erster wert ist die x achse zeiter die y
         interpolation = cv.INTER_AREA
         img1 = cv.resize(img,dim,interpolation) #  Anpassung der Bildgröße   
         return img1  
@@ -44,13 +44,14 @@ class DeepCar(CamCar):
             prepared_image = self.prepare_picture(img)
             # Bild übergeben und größe ändern
             resize_image = self.resize_picture(prepared_image)
-            
+            # zeigt das Bild an
             self.show_picture(resize_image)
-
-            self.steering_angle = self.calculate_angle_from_nn(resize_image)
+            # das verkleinerte bild an die winkelberechnung übergeben
             # Winkel setzen und Auto fahren lassen
-            self.drive(40,1)
-            #self.steering_angle = steering_angle
+            self.steering_angle = self.calculate_angle_from_nn(resize_image)
+            # Geschwindigkeit setzen
+            self.drive(30,1)
+            
         
         
         self.stop()
