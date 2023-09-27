6e7e86f23fbb289d96955af173ad28c64c3c99cd
HopfeJ <jan198686@gmx.de>
2023-09-27 18:58:49 +0200

    Jan

---
+++ Jan/Phase2/deepcar.py
@@ -0,0 +1,54 @@
+from camcar import CamCar
+import cv2 as cv
+import numpy as np
+from time import sleep
+from datetime import datetime
+import tensorflow as tf
+
+
+
+
+class DeepCar(CamCar):
+
+    def load_model(self):
+        path_to_model_file = './DEMO_MODEL'
+        self.model_loaded = tf.keras.models.load_model(path_to_model_file)
+        #model_loaded.layers[0].input_shape
+
+    def calculate_angle_from_nn(self, img):
+        angle = self.model_loaded(img).numpy()
+        print(angle)
+
+    def resize_picture(self,img):
+        dim = (320,150)
+        interpolation = cv.INTER_AREA
+        img1 = cv.resize(img,dim,interpolation) #  Anpassung der Bildgröße   
+        return img1  
+
+
+
+    def run(self):
+        self.load_model()
+        while True:
+            if self.stop_it: 
+                break
+            # Bild machen
+            img = self.make_picture()
+            # Bild schneiden und colorieren
+            prepared_image = self.prepare_picture(img)
+            # Bild übergeben und größe ändern
+            resize_image = self.resize_picture(prepared_image)
+            self.calculate_angle_from_nn(resize_image)
+            self.show_picture(resize_image)
+
+            #steering_angle = 
+            # Winkel setzen und Auto fahren lassen
+          
+            #self.steering_angle = steering_angle
+        
+        
+        self.stop()
+
+if __name__ == "__main__":
+    my_car = DeepCar()
+    my_car.run()
