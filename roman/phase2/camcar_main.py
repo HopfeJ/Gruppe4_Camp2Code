eff31d45d416c9fafe42119a8f4a2e4824483fa7
eisi82 <roman.eisenbarth@yahoo.com>
2023-09-27 11:59:30 +0200

    roman: auto fährt!

--- roman/phase2/camcar_main.py
+++ roman/phase2/camcar_main.py
@@ -12,23 +12,29 @@ car.steering_angle = 90
 
 # Video-Schleife
 while True:
-    img = car.get_prep_image()
+    
+    img = car.get_prep_image(100,370,0,640)
     line_segments = car.calc_line_segments(img)
-    print(line_segments)
+    #print(line_segments)
     
     if line_segments is not None:
-        linien = car.teile_linien(img, line_segments)
-        print('Avg Steigung und Schnittpunkt:')
-        print(linien)
+        linien = car.calc_fahrbahnlinien(img, line_segments)
         leitlinie = car.calc_leitlinie(img, linien)
         imageresult = car.draw_line_segments(linien, leitlinie, img)
-        lenkwinkel = car.calc_steering_angle(img, linien)
-        car.steering_angle = lenkwinkel
-        cv.imshow("Display window (press q to quit)", imageresult)
+        aktueller_lenkwinkel = car.steering_angle
+        errechneter_lenkwinkel = car.calc_steering_angle(img, linien)
+        print('aktueller Lenkwinkel:', aktueller_lenkwinkel, type(aktueller_lenkwinkel))
+        print('errechneter Lenkwinkel: ', errechneter_lenkwinkel, type(aktueller_lenkwinkel))
+
+        car.steering_angle = errechneter_lenkwinkel
+        print('gesetzter Lenkwinkel: ', car.steering_angle)
+        cv.namedWindow("MeinFenster", cv2.WINDOW_NORMAL)
+        cv.resizeWindow("MeinFenster", 800, 600)
+        cv.imshow("MeinFenster", imageresult)
         # Ende bei Drücken der Taste q
         if cv.waitKey(1) == ord('q'):
             break
-        time.sleep(0.3)
+        time.sleep(0.05)
     
     else:
         car.stop()
--- roman/phase2/camcar_new.py
+++ roman/phase2/camcar_new.py
@@ -17,13 +17,15 @@ class CamCar(BaseCar):
         self.cam = Camera()
     
 
-    def get_prep_image(self):
+    def get_prep_image(self,x1,x2,y1,y2):
         # Werte f+r Blau-Anteil im HSV-Farbraum (360 Grad / 2)
-        lower = np.array([90, 0, 0])
-        upper = np.array([125, 255, 255])
+        lower = np.array(self.data['lower'])
+        upper = np.array(self.data['upper'])
 
         img = self.cam.get_frame()                          # Foto aufnehmen
-        img_cut = img[200:400,20:620].copy()                # Foto beschneiden
+        print(img.shape)
+        img_cut = img[x1:x2,y1:y2].copy()                # Foto beschneiden [200:400,20:620]
+        #img_cut = img.copy()
         img_cut_HSV = cv.cvtColor(img_cut,cv.COLOR_BGR2HSV) # in HSV-Raum umwandeln
         image_mask = cv.inRange(img_cut_HSV, lower, upper)  # blau-Anteil maskieren
         image_edges = cv.Canny(image_mask,200,400)          # Kanten der Blau-Maske ermitteln
@@ -33,7 +35,7 @@ class CamCar(BaseCar):
     def calc_line_segments(self, image_edges):
         rho = 1              # distance precision in pixel, i.e. 1 pixel
         angle = np.pi / 180  # angular precision in radian, i.e. 1 degree
-        min_threshold = 10   # in etwas Anzal der Punkt auf der Geraden. Je geringer Min_threshold, desto mehr Geraden werden erkannt.
+        min_threshold = 20   # in etwas Anzal der Punkt auf der Geraden. Je geringer Min_threshold, desto mehr Geraden werden erkannt.
         minLineLength = 8    # Minimale Linienlänge
         maxLineGap = 4       # Maximale Anzahl von Lücken in der Linie
         line_segments = cv.HoughLinesP(image_edges, rho, angle, min_threshold, np.array([]), minLineLength=minLineLength, maxLineGap=maxLineGap)
@@ -43,19 +45,19 @@ class CamCar(BaseCar):
         höhe, breite = image_edges.shape
         slope, intercept = line
         y1 = höhe  # unterer Rand des Bildes
-        y2 = int(y1 * 0.25)  # erstelle Linie von 75% von oben bis nach unten
+        y2 = int(y1 * 0.6)  # erstelle Linie von 75% von oben bis nach unten: 0.25
 
         # errechne Koordinaten aus Steigung und SChnittpunk
         x1 = max(-breite, min(2 * breite, int((y1 - intercept) / slope)))
         x2 = max(-breite, min(2 * breite, int((y2 - intercept) / slope)))
         return [[x1, y1, x2, y2]]
 
-    def teile_linien(self, image_edges, line_segments):
+    def calc_fahrbahnlinien(self, image_edges, line_segments):
         
-        linien = []
+        fahrbahnlinien = []
         if line_segments is None:
-            print('teile_linien(): Es wurden keine Linien in line_segments gefunden.')
-            return linien
+            print('calc_fahrbahnlinien(): Es wurden keine Linien in line_segments gefunden.')
+            return fahrbahnlinien
 
         höhe, breite = image_edges.shape # Höhe und Breite des Bildes auslesen
         links_werte = []
@@ -68,86 +70,106 @@ class CamCar(BaseCar):
         for line_segment in line_segments:
             for x1, y1, x2, y2 in line_segment:
                 if x1 == x2:
-                    print('Vertikale Linie überspringen, da Steigung unendlich.')
+                    #print('Vertikale Linie überspringen, da Steigung unendlich.')
                     continue
-                werte = np.polyfit((x1, x2), (y1, y2), 1) # Zu gegebenen Koordinaten des line segments die Steigung und Schnittpunkt finden
+                # Was tun, wenn x2 < x1, sich also die Linien kreuzen?
+                werte = np.polyfit((x1, x2), (y1, y2), 1) # Zu gegebenen Koordinaten des line segments die Steigung und Schnittpunkt finden #(x1, x2), (y1, y2)
                 slope = werte[0]      # Steigung
                 intercept = werte[1]  # Schnittpunkt
+                #print('werte für Steigung und schnittpkt:', werte)
                 if slope < 0:       # Wenn Steiung negativ: Links
                     if x1 < linker_grenzbereich and x2 < linker_grenzbereich:
                         links_werte.append((slope, intercept))
                 else:               # Steigung positiv: Rechts
                     if x1 > rechter_grenzbereich and x2 > rechter_grenzbereich:
                         rechts_werte.append((slope, intercept))
-
+        # was tun, wenn links/rechts avg = nan, also keine Linie gefunden? -> maximaler Lenkeinschlag in Gegenrichtung?
+        
+        #print('links_werte: ', links_werte)
         links_werte_avg = np.average(links_werte, axis=0)
-        print('left avg: ', links_werte_avg)
-        if len(links_werte) > 0:
-            linien.append(self.calc_linie(image_edges, links_werte_avg))
-
+        #print(type(links_werte_avg))
+        #print('left avg: ', links_werte_avg)
+        if np.isnan(links_werte_avg).any():
+            #print('links_werte_avg nan')
+            default_links = [[1, höhe, -500, int(höhe * 0.6)]] # 0.5
+            fahrbahnlinien.append(default_links)
+        else:
+            fahrbahnlinien.append(self.calc_linie(image_edges, links_werte_avg))
+            
+        #print('rechts_werte: ', rechts_werte)
         rechts_werte_avg = np.average(rechts_werte, axis=0)
-        print('right avg: ', rechts_werte_avg)
-        if len(rechts_werte) > 0:
-            linien.append(self.calc_linie(image_edges, rechts_werte_avg))
-        return linien
+        if np.isnan(rechts_werte_avg).any():
+            #print('rechts_werte_avg nan')
+            default_rechts = [[breite, höhe, breite+500, int(höhe * 0.6)]] # 0.5
+            fahrbahnlinien.append(default_rechts)
+        else:
+            fahrbahnlinien.append(self.calc_linie(image_edges, rechts_werte_avg))
+
+        #print('calc_fahrbahnlinien(): ', fahrbahnlinien)
+        return fahrbahnlinien
     
-    def calc_leitlinie(self, img, linien):
+    def calc_leitlinie(self, img, fahrbahnlinien):
         # Leitlinie
         leitlinie = []
         höhe, breite = img.shape
 
-        print(leitlinie)
-        if linien[0][0]:
-            links = linien[0][0]
-            left_x2= links[2]
-            y2 = links[3]
+        if fahrbahnlinien[0][0]:
+            links = fahrbahnlinien[0][0]
 
-        if linien[1][0]:
-            rechts = linien[1][0]
-            right_x2 = rechts[2]
+            links_x2= links[2]
+            y2 = links[3]
+            #print('leitlinie links_x2, links_y2: ', links_x2, y2)
+
+        if fahrbahnlinien[1][0]:
+            rechts = fahrbahnlinien[1][0]
+
+            rechts_x2 = rechts[2]
             y2 = rechts[3]
+            #print('leitlinie rechts_x2, rechts_y2: ', rechts_x2, y2)
 
         mid = int(breite / 2)
-        x2_leitlinie = int((left_x2 + right_x2) / 2)
+        x2_leitlinie = int((links_x2 + rechts_x2) / 2)
         leitlinie = [[mid, höhe, x2_leitlinie, y2]] # 50 = y2 von links oder rechts
-        print(leitlinie)
+        #print(leitlinie)
         return leitlinie
     
-    def calc_steering_angle(self, frame, linien):
+    def calc_steering_angle(self, frame, fahrbahnlinien):
 
-        if len(linien) == 0:
+        if len(fahrbahnlinien) == 0:
             print('Keine Linien gefunden -> nicht lenken.')
             return -90
 
         höhe, breite = frame.shape
-        if len(linien) == 1:
-            print('Nur eine Linie gefunden. %s' % linien[0])
-            x1, _, x2, _ = linien[0][0]
+        if len(fahrbahnlinien) == 1:
+            print('Nur eine Linie gefunden. %s' % fahrbahnlinien[0])
+            x1, _, x2, _ = fahrbahnlinien[0][0]
             x_versatz = x2 - x1
         else:
-            links = linien[0][0]
-            rechts = linien[1][0]
-            left_x2= links[2]
-            right_x2 = rechts[2]
+            links = fahrbahnlinien[0][0]
+            rechts = fahrbahnlinien[1][0]
+            links_x2= links[2]
+            rechts_x2 = rechts[2]
             mid = int(breite / 2)
-            x_versatz = (left_x2 + right_x2) / 2 - mid
+            x_versatz = (links_x2 + rechts_x2) / 2 - mid
 
         # Lenkwinkel als Winkel zwischen Senkrechten auf X-Achse und Endpunkt der Leitlinie
         y_versatz = int(höhe / 2) # halbe Bildhöhe
 
-        winkel_zur_mitte_radian = math.atan(x_versatz / y_versatz)  
-        winkel_zur_mitte_grad = int(winkel_zur_mitte_radian * 180.0 / math.pi)
-        steering_angle = winkel_zur_mitte_grad + 90  # 90 als geradeaus
+        winkel_radian = math.atan(x_versatz / y_versatz)  
+        winkel_grad = int(winkel_radian * 180.0 / math.pi)
+        steering_angle = winkel_grad + 90  # + 90 als geradeaus
 
-        print('new steering angle: %s' % steering_angle)
+        #print('new steering angle: %s' % steering_angle)
         return steering_angle
+    
+    
 
     def draw_line_segments(self, line_segments, leitlinie, img):
         img2 = img.copy()
         img2 = cv.cvtColor(img2, cv.COLOR_GRAY2RGB)
         for line in line_segments:
             x1,y1,x2,y2 = line[0]
-            print(f'x1: {x1}, y1: {y1}, x2: {x2}, y2: {y2}')
+            #print(f'x1: {x1}, y1: {y1}, x2: {x2}, y2: {y2}')
             cv.line(img2,(x1,y1),(x2,y2),(200,100,100),3)
         if leitlinie is not None:
             cv.line(img2,(leitlinie[0][0], leitlinie[0][1]), (leitlinie[0][2], leitlinie[0][3]),(0,0,255),3) # Leitlinie einzeichnen
