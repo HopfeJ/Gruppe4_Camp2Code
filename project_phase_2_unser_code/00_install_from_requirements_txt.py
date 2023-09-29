09d470c1a2302347b39e508d175b257f480b60c7
itleicht <descartesbs@web.de>
2023-09-29 12:06:10 +0200

    requirements.txt nutzen HowTo

---
+++ project_phase_2_unser_code/00_install_from_requirements_txt.py
@@ -0,0 +1,6 @@
+    """
+        Alle benötigten Bibliotheken für ein Projekt lassen sich mit einem file requirements.txt installieren:
+        - Analog der Schreibweise des Aufrufs von "pip3 freeze", werden die benötigten Bibliotheken in requirements.txt geschrieben.
+        - Anschließend werden mit "pip install -r requirements.txt" die Pakete installiert.
+
+    """
